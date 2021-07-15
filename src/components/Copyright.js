2be0414767571802732c5bcea367e4d9b426ff45
PeterK <themingwe@gmail.com>
2021-07-15 18:22:20 +0300

    temp 1

--- src/components/Copyright.js
+++
@@ -1,8 +0,0 @@
-export const Copyright = () => {
-    return (
-        <div className="w-100 small pt-4 bg-white-transp">
-            <p>Created by Peter Mng using https://openweathermap.org/ API</p>
-            <p>source code on <a href="https://github.com/mingwe/CanISkii">github</a></p>
-        </div>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,49 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-import { Provider } from 'react-redux'
-import { applyMiddleware, createStore, compose } from 'redux'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from "redux-devtools-extension";
-
-import {rootReducer} from './reducers/rootReducer'
-
-
-// const devtoolsEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// const composed = compose(applyMiddleware(thunk), devtoolsEnhancer)
-const composed = composeWithDevTools(applyMiddleware(thunk))
-
-const preloadedState = {
-    places: [],
-    fetchedPlace: {},
-    city: {},
-    isLoading: true,
-    hasError: false,
-    globalTheme: {
-        bg: 'bg-init',
-        isInit: true,
-        isRain: false,
-        isCloud: false,
-        isGoodIdea: false
-    }
-}
-
-
-const store = createStore(rootReducer, preloadedState, composed)
+import Widget from './components/Widget';
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App store={store} />
-      </Provider>
+      <Widget/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
